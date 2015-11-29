695b4aeb1af8432ae53513137e8acc629e3d9749
Alin Badila <badila.alin@yahoo.com>
2015-11-29 19:29:50 +0200

    Added jquery lib, modified mapSetup.js with browser geolocation test.

--- js/mapSetup.js
+++ js/mapSetup.js
@@ -1,6 +1,12 @@
 var map;
+var browserSupportFlag = new Boolean();
+var query;
+var initialLocation;
+var lat;
+var lng;
+
 window.onload = function() {
-  var options = {
+  var mapOptions = {
     zoom:14,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     disableDefaultUI: true,
@@ -21,6 +27,42 @@ window.onload = function() {
     },
     scaleControl: true
   }
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-  map = new google.maps.Map(document.getElementById("map"), options)
+  $(document).ready(function() {
+    if (navigator.geolocation) {
+        browserSupportFlag = true;
+        navigator.geolocation.getCurrentPosition(function (position) {
+            initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            lat = position.coords.latitude;
+            lng = position.coords.longitude;
+            map.setCenter(initialLocation);
+            query = "ST_DISTANCE('Coordonate', LATLNG(" + lat + "," + lng + "))";
+        }, function () {
+            handleNoGeolocation(browserSupportFlag);
+        });
+    }
+    // Browser doesn't support Geolocation
+    else {
+        browserSupportFlag = false;
+        handleNoGeolocation(browserSupportFlag);
+    }
+
+    function handleNoGeolocation(errorFlag) {
+        if (errorFlag == true) {
+            alert("Geolocation service failed.");
+            lat = 44.55;
+            lng = 25.75;
+            initialLocation = new google.maps.LatLng(lat, lng);
+        } else {
+            alert("Your browser doesn't support geolocation.");
+            lat = 44.424911; //ISUBIF
+            lng = 26.102528;
+            initialLocation = new google.maps.LatLng(lat, lng);
+        }
+            map.setCenter(initialLocation);
+            query = "\"ST_DISTANCE('Coordonate', LATLNG(" + lat + "," + lng + "))\"";
+        }
+  });
+
 }
