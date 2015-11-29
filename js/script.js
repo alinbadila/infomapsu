fbf92ae40b35e435a5344f02681e503f8a8ec2fa
Alin Badila <badila.alin@yahoo.com>
2015-11-29 23:00:18 +0200

    Added search bar.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$('#menuDiv').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrain_width: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on click
+    alignment: 'right', // Aligns dropdown to left or right edge (works with constrain_width)
+    gutter: 0, // Spacing from edge
+    belowOrigin: false // Displays dropdown below the button
+});
+
+var focus = false;
+
+function toggleSearchBar() {
+    $('.searchdiv').slideToggle();
+    $('#pac-input').val('');
+    $('#pac-input').focus();
+};
+
+function hideAll() {
+    $('.dropdown-content').hide();
+}
